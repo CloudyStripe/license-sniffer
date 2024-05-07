ca55f2d617bca83a3cf8fd9fcb2b439ed8c301e9
Skyler Savard <ssavard@korterra.com>
2024-05-07 16:15:32 -0500

    finding package.json successfully.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-print('Hello, World!')
+import os
+
+manifestLocation = input("Enter the manifest file location: ")
+
+try:
+    with open(manifestLocation, "r") as file:
+
+        fileName = os.path.basename(file.name)
+        
+        if fileName != 'package.json':
+            print("File is not a package.json file")
+            exit()
+        if fileName == 'package.json':
+            print("Package.json located. Reading file...")
+            print(file.read())
+
+except Exception as e:
+    print(f"Invalid path: {e}")
+    exit()
