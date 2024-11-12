8b5ce5dc946ee8c604cd5cc17e048e09d370804e
Skyler Savard <ssavard@korterra.com>
2024-11-12 12:59:04 -0600

    code moved. need additional testing. add more comments in util function as well.

--- src/scanners/node/nodeScanner.py
+++ src/scanners/node/nodeScanner.py
@@ -2,6 +2,7 @@ from collections import defaultdict, deque
 import csv
 import json
 import os
+from scanners.utils.licenseCategorize import categorizeLicense 
 
 class NodeScanner:
 
@@ -131,53 +132,8 @@ class NodeScanner:
         return self.license_collection
     
     def _categorize_license(self, license_name):
-        ##REMINDER! Move this to a separate file.
-        open_source_licenses = [
-            'MIT', 'Apache-2.0', 'GPL-3.0', 'BSD', 'BSD-3-Clause', 'ISC',
-            'BSD-2-Clause', 'Python-2.0', 'CC0-1.0', 'CC-BY-4.0', '0BSD', 'CC-BY-3.0', 'OFL-1.1', 'MPL-2.0'
-        ]
-        closed_source_licenses = ['Proprietary']
-        partial_licenses = ['LGPL-2.1', 'LGPL-3.0']
-        unlicensed_licenses = ['UNLICENSED', 'Unlicense']
-        eula_licenses = ['SEE LICENSE IN EULA_MICROSOFT VISUAL STUDIO TEAM SERVICES AUTHHELPER FOR NPM.txt']
-
-        # Handle multiple licenses
-        if 'AND' in license_name or 'OR' in license_name:
-            licenses = license_name.replace('AND', 'OR').split('OR')
-            categories = set()
-            for lic in licenses:
-                lic = lic.strip().strip('()')
-                if lic in open_source_licenses:
-                    categories.add('Open Source')
-                elif lic in closed_source_licenses:
-                    categories.add('Closed Source')
-                elif lic in partial_licenses:
-                    categories.add('Partial')
-                elif lic in unlicensed_licenses:
-                    categories.add('Unlicensed')
-                elif lic in eula_licenses:
-                    categories.add('EULA')
-                else:
-                    categories.add('Unknown')
+        return categorizeLicense(license_name)
         
-            if len(categories) == 1:
-                return categories.pop()  # If all licenses fall into the same category. Sets cannot be indexed.
-            else:
-                return 'Mixed'
-
-        if license_name in open_source_licenses:
-            return 'Open Source'
-        elif license_name in closed_source_licenses:
-            return 'Closed Source'
-        elif license_name in partial_licenses:
-            return 'Partial'
-        elif license_name in unlicensed_licenses:
-            return 'Unlicensed'
-        elif license_name in eula_licenses:
-            return 'EULA'
-        else:
-            return 'Unknown'
-
 
     def export_license_csv(self):
         # Initialize the category count dictionary
